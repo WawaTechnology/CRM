f45fed20923e3eff28aaabbb8e152565bba8e77a
Sandor <470193852@163.com>
2019-08-19 15:10:59 +0800

    minorchange_sandor

--- server/api/login/services.js
+++ server/api/login/services.js
@@ -4,8 +4,7 @@ const User = require('../user/model')
 module.exports = {
     validateUser: async (username, password) => {
         let user = await User.findOne({name: username}, 'password')
-        console.log('=====================')
-        console.log(user)
+        
         return user.password === password
     }
 }
--- server/configuration/pro.js
+++ server/configuration/pro.js
@@ -1,4 +1,4 @@
 module.exports = {
     databaseAddress:'mongodb://127.0.0.1:27017/CRM',  //数据库地址请自行修改
-    port:3000
+    port:2080
 }
